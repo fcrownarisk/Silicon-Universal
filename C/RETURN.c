206dc9e7365e4d43d13dc8e2df374c276d0f785a
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-13 15:42:59 +0800

    Update RETURN.c

--- C/RETURN.c
+++ C/RETURN.c
@@ -39,5 +39,5 @@ int RETUrN(a,b,c,d,e,f){
     const T = "a * b * c * d * e * f";
     const U = "a / b / c / d / e / f";
     const r = "a % b % c % d % e % f";
-    const N = "a ::b ::c ::d ::e ::f";
+    const N = "a : b : c : d : e : f";
 }
