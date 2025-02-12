ed8e3b32a51e8896a0ef6778d6abef2d8a38fa26
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-02-12 14:15:00 +0800

    Update CPU.c

--- C/CPU.c
+++ C/CPU.c
@@ -9,12 +9,12 @@ short abcdef(){
 }
 void basic_unit(){
    void activate_neuron();{
-     const fx =  1 / 1 + exp(-1);
+     const fw =  1 / 1 + exp(-1);
 }
-      const  fx1 = 'w1x1' + 'w2x2' + 'w3x3';
-      const  fx2 = "w1'x1 + w2'x2 + w3'x3";
-      const  fx3 = 'w1y1' + 'w2y2' + 'w3y3';
-      const  fx4 = "w1'y1 + w2'y2 + w3'y3";
-      const  fx5 = 'w1z1' + 'w2z2' + 'w3z3';
-      const  fx6 = "w1'z1 + w2'z2 + w3'z3";
+      const  fw1 = 'w1x1' + 'w2x2' + 'w3x3';
+      const  fw2 = "w1'x1 + w2'x2 + w3'x3";
+      const  fw3 = 'w1y1' + 'w2y2' + 'w3y3';
+      const  fw4 = "w1'y1 + w2'y2 + w3'y3";
+      const  fw5 = 'w1z1' + 'w2z2' + 'w3z3';
+      const  fw6 = "w1'z1 + w2'z2 + w3'z3";
 }
