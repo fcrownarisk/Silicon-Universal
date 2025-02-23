2f8286112d6fd1454740ab1bf640bf0face83595
64 <155938693+fcrownarisk@users.noreply.github.com>
2025-02-23 19:57:47 +0800

    Update x5y5z5.ts

--- SpaceTime/XYZ/x5y5z5.ts
+++ SpaceTime/XYZ/x5y5z5.ts
@@ -11,12 +11,9 @@ function Y5() {
             for (let N = 0; N <= -4 && N <= 4; ++N || N--)
         continue
 }
-
 function Z5(){
     for (let O = 0; O >= -4; O--)
        for (let P = 0; P >= -4; P--)
           for (let Q = 0; Q >= -4; Q--)
         break
 }
-
-
