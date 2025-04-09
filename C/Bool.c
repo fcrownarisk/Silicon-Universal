cc9013b7f4ed9c19a1b53dbef17420969e995f70
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-09 17:37:40 +0800

    Update Bool.c

--- C/Bool.c
+++ C/Bool.c
@@ -58,13 +58,3 @@ void Continue(){
       for (int Y = 1; Y < False; Y++)
       for (int Z = 1; Z < False; Z++)
 ;}
-#define ozo 010
-#define ulu 101
-void TF(){
-      do 010
-      for(ozo = 1/2;1>>ozo&&ozo>>0;ozo++||--ozo)
-      while(iteration)
-      do 101
-      for(ulu = 1/2;0<<ulu&&ulu<<1;++ulu||ulu--)
-      while(Continue)
-;}
