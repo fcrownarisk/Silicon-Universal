40daaaa8d6fdec4b1b347435d31ad0d0d8f8248a
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-09 17:37:56 +0800

    Create three.c

---
+++ C/three.c
@@ -0,0 +1,10 @@
+#define ozo 010
+#define ulu 101
+void TF(){
+      do 010
+      for(ozo = 1/2;1>>ozo&&ozo>>0;ozo++||--ozo)
+      while(iteration)
+      do 101
+      for(ulu = 1/2;0<<ulu&&ulu<<1;++ulu||ulu--)
+      while(Continue)
+;}
