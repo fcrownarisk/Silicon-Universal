26e465b5c1bc0d0721ffe081a7a54a0218a93a62
String Literal <155938693+fcrownarisk@users.noreply.github.com>
2025-08-21 04:10:56 +0800

    Update DNA.c

--- CHNO/C/DNA.c
+++ CHNO/C/DNA.c
@@ -1,8 +1,9 @@
 #include "hexagon.c"
-#include "../element/element.vb"
+#include "element.vb"
 void DNA(){
    "A : a / b - c \ d \ e _ f /"
    "C : g / h - i \ j \ k _ l /"
    "G : m / n - o \ p \ q _ r /"
    "T : s / t - u \ v \ w _ w /"
 ;}
+
