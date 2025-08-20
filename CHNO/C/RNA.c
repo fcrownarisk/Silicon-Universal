6145b2ad7a83e46fe247524b7c97aae87b78fc8e
String Literal <155938693+fcrownarisk@users.noreply.github.com>
2025-08-21 04:11:06 +0800

    Update RNA.c

--- CHNO/C/RNA.c
+++ CHNO/C/RNA.c
@@ -1,8 +1,9 @@
 #include "pentagon.c"
-#include "../element/element.vb"
+#include "element.vb"
 void RNA(){
    "A : a / b \ c \ d _ e /"
    "C : f / g \ i \ j _ k /" 
    "G : l / m \ n \ o _ p /"
    "U : x | y | z"
 ;}
+
